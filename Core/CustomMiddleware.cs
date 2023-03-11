b09fcbafaa6f0437ea2a03e8885663bc4d57527b
Hihiz <public@mail.ru>
2023-03-11 20:09:29 +0300

    AddScoped

--- Core/CustomMiddleware.cs
+++ Core/CustomMiddleware.cs
@@ -1,6 +1,4 @@
 ﻿using Core.Services;
-using Microsoft.Extensions.Options;
-using System.Runtime.Serialization;
 
 namespace Core
 {
@@ -9,17 +7,18 @@ namespace Core
         private RequestDelegate _next;
         private IResponseFormatter _formatter;
 
-        public CustomMiddleware(RequestDelegate next, IResponseFormatter formatter)
+        public CustomMiddleware(RequestDelegate next)
         {
             _next = next;
-            _formatter = formatter;
         }
 
-        public async Task Invoke(HttpContext context)
+        public async Task Invoke(HttpContext context, IResponseFormatter formatter1, IResponseFormatter formatter2, IResponseFormatter formatter3)
         {
             if (context.Request.Path == "/middleware")
             {
-                await _formatter.Format(context, "Custom Middleware");
+                await formatter1.Format(context, string.Empty);
+                await formatter2.Format(context, string.Empty);
+                await formatter3.Format(context, string.Empty);
             }
             else
             {
