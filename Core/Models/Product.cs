8c607eefa75ca0758826462e678c41d90a77fbfe
Hihiz <public@mail.ru>
2023-03-12 19:28:22 +0300

    Создание моделей данных

---
+++ Core/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.Eventing.Reader;
+
+namespace Core.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        [Column(TypeName = "decimal(8,2)")]
+        public decimal Price { get; set; }
+
+        public long CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
