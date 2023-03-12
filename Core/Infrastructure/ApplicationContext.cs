fd80c0132059b127b5300f3a9707b9ac7b2c4e49
Hihiz <public@mail.ru>
2023-03-12 19:39:24 +0300

    ApplicationContext класс

---
+++ Core/Infrastructure/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Infrastructure
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
