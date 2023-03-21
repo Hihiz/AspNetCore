97b59847c68df1a941cbcbc8f414ecce48191d24
Hihiz <public@mail.ru>
2023-03-21 22:56:24 +0300

    ViewComponent

---
+++ Core/Components/ProductListingViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Core.Infrastructure;
+using Core.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.Components
+{
+    [ViewComponent(Name = "JuicyFruit")]
+    public class ProductListingViewComponent : ViewComponent
+    {
+        private ApplicationContext _db;
+
+        public IEnumerable<Product> Products;
+
+        public ProductListingViewComponent(ApplicationContext db)
+        {
+            _db = db;
+        }
+
+        public string Invoke()
+        {
+            return $"There are {_db.Products.Count()} products";
+        }
+    }
+}
