81aaa272f86ed49ceb9251155d4183739cabfd43
Hihiz <public@mail.ru>
2023-03-21 20:21:28 +0300

    Использование частичных представлений на страницах Razor

---
+++ Core/Pages/Shared/_ProductPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Core.Pages.Shared
+{
+    public class _ProductPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
