b1c731ecf6c22ee1e46e1e9d2f1386a926871adc
Hihiz <public@mail.ru>
2023-03-23 23:56:42 +0300

    FormController

---
+++ Core/Controllers/FormController.cs
@@ -0,0 +1,36 @@
+﻿using Core.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.Controllers
+{
+    public class FormController : Controller
+    {
+        private ApplicationContext _db;
+
+        public FormController(ApplicationContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index(long id = 1)
+        {
+            return View(await _db.Products.FindAsync(id));
+        }
+
+        [HttpPost]
+        public IActionResult SubmitForm()
+        {
+            foreach (string key in Request.Form.Keys)
+            {
+                TempData[key] = string.Join(", ", Request.Form[key]);
+            }
+
+            return RedirectToAction("Results");
+        }
+
+        public IActionResult Results()
+        {
+            return View();
+        }
+    }
+}
