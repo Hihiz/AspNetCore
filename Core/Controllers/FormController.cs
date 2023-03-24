5896b845ceefbf6833448f8f1a2db3cf2d7e938c
Hihiz <public@mail.ru>
2023-03-24 18:50:53 +0300

    Указание источника привязки модели 3

--- Core/Controllers/FormController.cs
+++ Core/Controllers/FormController.cs
@@ -42,5 +42,12 @@ namespace Core.Controllers
         {
             return $"Header: {accept}";
         }
+
+        [HttpPost]
+        [IgnoreAntiforgeryToken]
+        public Product Body([FromBody] Product model)
+        {
+            return model;
+        }
     }
 }
