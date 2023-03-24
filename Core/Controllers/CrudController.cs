19d4d345b38934670701a96baccc04979f56e5b8
Hihiz <public@mail.ru>
2023-03-24 20:52:08 +0300

    CrudController и индекс

---
+++ Core/Controllers/CrudController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Core.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Controllers
+{
+    public class CrudController : Controller
+    {
+        private ApplicationContext _db;
+
+        public CrudController(ApplicationContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index() => View(_db.Products.Include(c => c.Category));
+
+    }
+}
