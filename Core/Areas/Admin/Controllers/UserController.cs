f3addbdf6908709059d91de4adc0753fb063e274
Hihiz <public@mail.ru>
2023-03-25 19:51:33 +0300

    Список пользователей

--- Core/Areas/Admin/Controllers/UserController.cs
+++ Core/Areas/Admin/Controllers/UserController.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Core.Areas.Admin.Controllers
 {
     public class UserController : Controller
     {
+        private UserManager<IdentityUser> _userManager;
+
+        public UserController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [Area("Admin")]
         public IActionResult Index()
         {
-            return View();
+            return View(_userManager.Users.ToList());
         }
     }
 }
