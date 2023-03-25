5bd06938c4e9e0bfcd82b539355983bb6ab42907
Hihiz <public@mail.ru>
2023-03-25 22:43:15 +0300

    Авторизация

--- Core/Controllers/AccountController.cs
+++ Core/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using Core.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace Core.Controllers
 {
@@ -15,45 +17,45 @@ namespace Core.Controllers
             _userManager = userManager;
         }
 
-        public IActionResult Login(string returnUrl) => View(returnUrl);
-
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel loginVM)
         {
             if (ModelState.IsValid)
             {
                 Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(loginVM.UserName, loginVM.Password, false, false);
-
                 if (result.Succeeded)
                 {
                     return Redirect(loginVM.ReturnUrl ?? "/");
                 }
-
                 ModelState.AddModelError("", "Invalid username or password");
             }
-
             return View(loginVM);
         }
-
-        public IActionResult Details() => View(new AuthDetailsViewModel { Cookie = Request.Cookies[".AspNetCore.Identity.Application"] });
-
         public async Task<IActionResult> Details()
         {
             if (User.Identity != null && User.Identity.IsAuthenticated)
             {
                 IdentityUser user = await _userManager.FindByNameAsync(User.Identity.Name);
-
                 return View(new AuthDetailsViewModel { Cookie = Request.Cookies[".AspNetCore.Identity.Application"], User = user });
             }
-
             return View(new AuthDetailsViewModel());
         }
-
         public async Task<RedirectResult> Logout(string returnUrl = "/")
         {
             await _signInManager.SignOutAsync();
 
             return Redirect(returnUrl);
         }
+
+        [Authorize]
+        public string AllRoles() => "All Roles";
+
+        [Authorize(Roles = "Admin")]
+        public string AdminOnly() => "Admin Only";
+
+        [Authorize(Roles = "Manager")]
+        public string ManagerOnly() => "Manager Only";
+
+        public string AccessDenied() => "Access Denied";
     }
 }
