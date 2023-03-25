e426ef0eb896bbac7f6df6dfd49cf5a4c16ce636
Hihiz <public@mail.ru>
2023-03-25 20:41:13 +0300

    User Post

--- Core/Areas/Admin/Controllers/UserController.cs
+++ Core/Areas/Admin/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Core.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Core.Areas.Admin.Controllers
@@ -13,9 +14,31 @@ namespace Core.Areas.Admin.Controllers
             _userManager = userManager;
         }
 
-
         public IActionResult Index() => View(_userManager.Users.ToList());
 
         public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityUser newUser = new IdentityUser { UserName = user.UserName, Email = user.Email };
+                IdentityResult result = await _userManager.CreateAsync(newUser, user.Password);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+
+            }
+
+            return View(user);
+        }
     }
 }
